a808ecfe8b3e63983b21c5b71d179e974f81c77a
Radosław Dubiel <radieus@gmail.com>
2021-03-24 19:04:37 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace acyclic_coloring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
