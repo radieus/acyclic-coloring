af22c96a72722834c0cbb4bbedb31ddd93471305
Mikołaj Hościło <mikolajhoscilo@gmail.com>
2021-05-16 20:24:48 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,23 +17,23 @@ namespace acyclic_coloring
             g1.PrintIfCyclic();
             System.Console.WriteLine(g1.WelshPowellAlgorithm());
 
-            // Graph g2 = new Graph(3);
-            // g2.addEdge(0, 1);
-            // g2.addEdge(1, 2);
-            // g2.PrintIfCyclic();
+            Graph g2 = new Graph(3);
+            g2.addEdge(0, 1);
+            g2.addEdge(1, 2);
+            g2.PrintIfCyclic();
 
-            // Graph g3 = new Graph(2);
-            // g3.addEdge(0, 1);
-            // g3.addEdge(1, 0);
-            // g3.PrintIfCyclic();
+            Graph g3 = new Graph(2);
+            g3.addEdge(0, 1);
+            g3.addEdge(1, 0);
+            g3.PrintIfCyclic();
 
-            // Graph g4 = new Graph(4);
-            // g4.addEdge(2, 1);
-            // g4.addEdge(1, 3);
-            // g4.addEdge(0, 3);
-            // g4.addEdge(0, 1);
-            // g4.printGraph();
-            // g4.PrintIfCyclic();
+            Graph g4 = new Graph(4);
+            g4.addEdge(2, 1);
+            g4.addEdge(1, 3);
+            g4.addEdge(0, 3);
+            g4.addEdge(0, 1);
+            g4.printGraph();
+            g4.PrintIfCyclic();
 
             g4.colors[0] = 0;
             g4.colors[1] = 1;
