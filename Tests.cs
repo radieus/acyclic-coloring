5164f689ef6af05d7aa29a6966aff07867bc9392
Radosław Dubiel <radieus@gmail.com>
2021-05-17 01:31:48 +0200

    Refactor testing v2

--- Tests.cs
+++ Tests.cs
@@ -19,30 +19,6 @@ namespace acyclic_coloring
         }
 
         [Fact]
-        public void Test3()
-        {
-            Graph g5 = new Graph(9);
-            g5.addEdge(0, 1);
-            g5.addEdge(0, 3);
-            g5.addEdge(1, 2);
-            g5.addEdge(1, 3);
-            g5.addEdge(1, 4);
-            g5.addEdge(1, 5);
-            g5.addEdge(2, 5);
-            g5.addEdge(3, 6);
-            g5.addEdge(3, 4); // this edge makes it chordal
-            g5.addEdge(4, 5);
-            g5.addEdge(4, 6);
-            g5.addEdge(4, 7);
-            g5.addEdge(4, 8);
-            g5.addEdge(5, 8);
-            g5.addEdge(7, 8);
-
-            Assert.Equal(3, g5.WelshPowellAlgorithm());
-            Assert.True(g5.isProperCyclicColoring());
-        }
-
-        [Fact]
         public void TestG1()
         {
             Graph g1 = new Graph(5);
